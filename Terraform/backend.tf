c5fb43ed088411aabed709fb2dfa338f8a79cf4d
MarwanTamerMo <marwantamermo@gmail.com>
2025-08-17 19:45:17 +0300

    I hope this works

--- Terraform/backend.tf
+++ Terraform/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket         = "terraforms3marobucket"
-    key            = "prod/terraform.tfstate"
+    key            = "prod/terraform.tfstate" #hi
     region         = "eu-central-1"
     dynamodb_table = "terraform-locks"
     encrypt        = true
